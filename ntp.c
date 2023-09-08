4aaf21c0c68e69a0ba6bc9e84cb92fc755d120e6
Nauris Vasilevskis <nafris@inbox.lv>
2023-09-08 12:37:07 +0100

    working with possible improvements

--- ntp.c
+++ ntp.c
@@ -1,4 +1,9 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
 #include <stdio.h>
+#include <string.h>
 void ntpdate(char *host);
 int main(int argc, char *argv[]){
 	printf("You have passed %d arguments\n", argc);
@@ -20,7 +25,41 @@ int main(int argc, char *argv[]){
 void ntpdate(char *host){
 	//printf("Hostname from function: %s",host);
 	int port=123; //NTP port
-	int maxlen=1024 //buffer lenght
+	int maxlen=1024; //buffer lenght
 	int i; //workhorse
+	unsigned char msg[48]={010,0,0,0,0,0,0,0,0};
+	unsigned long buf[maxlen]; //buffer for replies from server
+	struct protoent *proto;
+	struct sockaddr_in server_addr;
+	int s; //socket
+	int tmit;
+	proto=getprotobyname("udp"); //returns protocol number
+	s=socket(PF_INET, SOCK_DGRAM, proto->p_proto);
+	perror("socket");
+	memset(&server_addr,0,sizeof(server_addr));
+	server_addr.sin_family=AF_INET;
+	server_addr.sin_addr.s_addr=inet_addr(host);
+	server_addr.sin_port=htons(port);
+	printf("sending data...\n");
+	i=sendto(s,msg,sizeof(msg),0,(struct sockaddr *)&server_addr,sizeof(server_addr));
+	perror("sendto");
+	//reading data back
+//	struct sockaddr saddr;
+//	socklen_t saddr_l = sizeof(saddr);
+//	i=recvfrom(s,buf,48,0,&saddr,&saddr_l);
+//	perror("recvfr:");
+//	tmit=ntohl((time_t)buf[10]); //getting transmit time
+//	tmit-=2208988800U;
+//	printf("Time: %s",ctime(&tmit));
+
+
+
+	struct sockaddr saddr;
+	socklen_t saddr_l = sizeof (saddr);
+	i=recvfrom(s,buf,48,0,&saddr,&saddr_l);
+	perror("recvfr:");
+	tmit=ntohl((time_t)buf[10]);    //# get transmit time
+	tmit-= 2208988800U;
+	printf("Time: %s",ctime(&tmit));
 	
 }
