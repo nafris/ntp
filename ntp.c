4cab4b3de8f58ced511dafc8e2851d44190efaa9
Nauris Vasilevskis <nafris@inbox.lv>
2023-09-15 07:31:35 +0100

    some formatting done

--- ntp.c
+++ ntp.c
@@ -4,8 +4,10 @@
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
+
 void ntpdate(char *host);
 void print_packet(char p[48]);
+
 int main(int argc, char *argv[]){
 	printf("You have passed %d arguments\n", argc);
 	for (int i=0; i<argc; i++){
@@ -15,14 +17,14 @@ int main(int argc, char *argv[]){
 	if (argc>1){ //host address has been passed via parameter
 		hostname=argv[1];
 		printf("Hostname from command line argument: %s \n",hostname);
-	}
-	else { //using hardcoded host address
+	}else { //using hardcoded host address
 		hostname="129.6.15.28";
 		printf("Using hardcoded hostname: %s \n",hostname);
 	}
 	ntpdate(hostname);
 	return 0;
 }
+
 void print_packet(char p[48]){
 		for (int off = 0; off < 48; off++) {
         	if (!(off % 0x10))
@@ -32,16 +34,16 @@ void print_packet(char p[48]){
 	printf("\n");
 }
 void ntpdate(char *host){
-	//printf("Hostname from function: %s",host);
 	int port=123; //NTP port
 	int maxlen=1024; //buffer lenght
-	int i; //workhorse
+	//int i; //workhorse
 	unsigned char msg[48]={8,0,0,0,0,0,0,0,0};
 	unsigned char buf[maxlen]; //buffer for replies from server
 	struct protoent *proto;
 	struct sockaddr_in server_addr;
 	int s; //socket
 	time_t tmit;
+	
 	proto=getprotobyname("udp"); //returns protocol number
 	s=socket(PF_INET, SOCK_DGRAM, proto->p_proto);
 	perror("socket");
@@ -51,11 +53,11 @@ void ntpdate(char *host){
 	server_addr.sin_port=htons(port);
 	printf("sending data...\n");
 	print_packet(msg);
-	i=sendto(s,msg,sizeof(msg),0,(struct sockaddr *)&server_addr,sizeof(server_addr));
+	sendto(s,msg,sizeof(msg),0,(struct sockaddr *)&server_addr,sizeof(server_addr));
 	perror("sendto");
 	struct sockaddr saddr;
 	socklen_t saddr_l = sizeof (saddr);
-	i=recvfrom(s,buf,48,0,&saddr,&saddr_l);
+	recvfrom(s,buf,48,0,&saddr,&saddr_l);
 	perror("recvfr:");
 	print_packet(buf);
 	tmit=ntohl((time_t)buf[10]);    //# get transmit time
